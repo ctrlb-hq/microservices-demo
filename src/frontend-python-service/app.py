aa5efcd4e6e0ef1fadbcc610402ef9a55ab31052
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2023-04-15 11:53:55 +0000

    Completed python service

---
+++ src/frontend-python-service/app.py
@@ -0,0 +1,58 @@
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, request
+import os
+import uuid
+import requests
+
+PYTHON_SERVICE_HOST = os.getenv("PYTHON_SERVICE_HOST","0.0.0.0")
+PYTHON_SERVICE_PORT = os.getenv("PYTHON_SERVICE_PORT","60000")
+
+
+JAVA_SERVICE_HOST = os.getenv("JAVA_SERVICE_HOST","0.0.0.0")
+JAVA_SERVICE_PORT = os.getenv("JAVA_SERVICE_PORT","60001")
+
+KAFKA_SERVICE_HOST = os.getenv("KAFKA_SERVICE_HOST","0.0.0.0")
+KAFKA_SERVICE_PORT = os.getenv("KAFKA_SERVICE_PORT","9092")
+KAFKA_SERVICE_TOPIC = os.getenv("KAFKA_SERVICE_TOPIC","first_topic")
+
+app = Flask(__name__)
+
+from kafka import KafkaProducer
+producer = KafkaProducer(
+    bootstrap_servers=[f"{KAFKA_SERVICE_HOST}:{KAFKA_SERVICE_PORT}"],
+    value_serializer=lambda x: dumps(x).encode('utf-8')
+)
+
+"""
+Frontend calls this function with a get parameter 'number'
+This function puts this on kafka and gives back a uuid for the result 
+"""
+@app.route('/')
+def home():
+    number = request.args.get('number')
+    uid = str(uuid.uuid4())
+    data = {'uuid': uid, 'number':number}
+    producer.send(KAFKA_SERVICE_TOPIC, value=data)
+    return str(f"Your request has been registered against {uid}")
+
+"""
+This function calls the Java service endpoint /numbers with uuid parameter as obtained here
+Then prints the result.
+"""
+@app.route('/getResult')
+def result:
+    uid = request.args.get('uuid')
+    PARAMS = {'uuid':uid}
+
+    # sending get request and saving the response as response object
+    r = requests.get(url = f"{JAVA_SERVICE_HOST}:{JAVA_SERVICE_PORT}/numbers", params = PARAMS)
+    # extracting data in json format
+    data = r.json()
+    return str(f"Your response is: {data}")
+
+
+# main driver function
+if __name__ == '__main__':
+
+	app.run(host=PYTHON_SERVICE_HOST, port=PYTHON_SERVICE_PORT)
