c628014f1dddb115243fcefa5eae253962a12b95
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2023-04-22 17:04:48 +0000

    Added go to python to go route

--- src/frontend-python-service/app.py
+++ src/frontend-python-service/app.py
@@ -12,6 +12,9 @@ PYTHON_SERVICE_PORT = os.getenv("PYTHON_SERVICE_PORT","30000")
 JAVA_SERVICE_HOST = os.getenv("JAVA_SERVICE_HOST","0.0.0.0")
 JAVA_SERVICE_PORT = os.getenv("JAVA_SERVICE_PORT","30001")
 
+GO_SERVICE_HOST = os.getenv("GO_SERVICE_HOST","0.0.0.0")
+GO_SERVICE_PORT = os.getenv("GO_SERVICE_PORT","30002")
+
 KAFKA_SERVICE_HOST = os.getenv("KAFKA_SERVICE_HOST","0.0.0.0")
 KAFKA_SERVICE_PORT = os.getenv("KAFKA_SERVICE_PORT","9092")
 KAFKA_SERVICE_TOPIC = os.getenv("KAFKA_SERVICE_TOPIC","test")
@@ -51,8 +54,30 @@ def result():
     data = r.json()
     return str(f"Your response is: {data}")
 
+"""
+This function calls the Java service endpoint /numbers with uuid parameter as obtained here
+Then prints the result.
+"""
+@app.route('/helloGo')
+def helloGo():
+    # sending get request and saving the response as response object
+    r = requests.get(url = f"http://{GO_SERVICE_HOST}:{GO_SERVICE_PORT}/ping/")
+    # extracting data in json format
+    print(r)
+    return str(f"Your response is: {r.text}")
+
+"""
+This function calls the Java service endpoint /numbers with uuid parameter as obtained here
+Then prints the result.
+"""
+@app.route('/ping')
+def ping():
+    # sending get request and saving the response as response object
+    r = requests.get(url = f"http://{GO_SERVICE_HOST}:{GO_SERVICE_PORT}/ping/")
+    # extracting data in json format
+    print(r)
+    return str(f"Your response is: {r.text}")
 
 # main driver function
 if __name__ == '__main__':
-
 	app.run(host="0.0.0.0", port=PYTHON_SERVICE_PORT)
