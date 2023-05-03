be19a6343e53a7fe20b56867057a5ec292c85d53
Mayank Singh Chauhan <mayanksingh2298@gmail.com>
2023-05-03 22:38:49 +0530

    Update NumberService.java

--- src/java-service/src/main/java/com/example/demo/NumberService.java
+++ src/java-service/src/main/java/com/example/demo/NumberService.java
@@ -30,7 +30,7 @@ public class NumberService {
 
     public NumberEntity processNumber(NumberEntity number) {
         int num = number.getNumber();
-        number.setNumber(num * num * num);
+        number.setNumber(num * num);
         return number;
     }
 }
